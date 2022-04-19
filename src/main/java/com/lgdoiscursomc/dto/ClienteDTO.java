6e4a2ddbe5e03a77df8e10f4b3774d262151bc51
ASAngelica <asa196549@gmail.com>
2022-04-19 17:58:15 -0300

    Validacao customizada: email nao repetido na atualizacao do Cliente

--- src/main/java/com/lgdoiscursomc/dto/ClienteDTO.java
+++ src/main/java/com/lgdoiscursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.lgdoiscursomc.domain.Cliente;
+import com.lgdoiscursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
