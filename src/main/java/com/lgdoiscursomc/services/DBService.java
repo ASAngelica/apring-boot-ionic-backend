59e22f236ba4a9163c758fca25f3a1d95ca0b3ed
ASAngelica <asa196549@gmail.com>
2022-05-04 17:34:28 -0300

    Adicionando senha a Cliente

--- src/main/java/com/lgdoiscursomc/services/DBService.java
+++ src/main/java/com/lgdoiscursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.lgdoiscursomc.domain.Categoria;
@@ -34,6 +35,9 @@ import com.lgdoiscursomc.repositories.ProdutoRepository;
 public class DBService {
 	
 	//dependências
+	
+		@Autowired
+		private BCryptPasswordEncoder pe;
 	
 		@Autowired
 		private CategoriaRepository categoriaRepository;
@@ -129,7 +133,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "asa196549@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "asa196549@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
